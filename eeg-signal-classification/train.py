c33d1425b6e036a367b5a8862b034db05b35e90e
mikhail.shuravin <mashuravin@edu.hse.ru>
2024-01-08 21:34:14 +0300

    junk # rows removed, pyproject name changed

--- eeg-signal-classification/train.py
+++ eeg-signal-classification/train.py
@@ -20,9 +20,6 @@ def start_train(df_all_str, iterations, learning_rate, depth):
         df_all_X, df_all_y, test_size=0.05, shuffle=False, random_state=42
     )
 
-    # print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
-
-    # defaults are 20 0.5 7
     model = CatBoostClassifier(
         iterations=iterations, learning_rate=learning_rate, depth=depth
     )
@@ -30,15 +27,8 @@ def start_train(df_all_str, iterations, learning_rate, depth):
     model.fit(X_train, y_train)
 
     preds_class = model.predict(X_test)
-    # preds_proba = model.predict_proba(X_test)
-
     accuracy = accuracy_score(y_test, preds_class)
     print("Accuracy:", accuracy)
-
-    # print("Pred. classes:\n", preds_class[:9])
-    # print("Pred. proba:\n", preds_proba[:9])
-    # print("Actual:   ", list(y_test[:9]))
-    # print("Predicted:", list(preds_class[:9]))
 
     logloss = model.get_evals_result()
     print("Log loss:", logloss["learn"]["Logloss"][-1])
